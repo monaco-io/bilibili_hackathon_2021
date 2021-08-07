8b1f9601e1a4d320406a51ffadc20c573f4f008a
卢旭泽 <luxuze@icloud.com>
2021-08-07 10:52:57 +0800

    add

--- router/flask.py
+++ router/flask.py
@@ -1,8 +1,35 @@
+import os
+
+from kawayi import Anime
+
 from flask import Flask
 
 app = Flask(__name__)
 
+anime = Anime()
+
+image_folder = os.path.join(
+    os.path.dirname(
+        os.path.realpath(__file__)),
+    "../image"
+)
+
 
 @app.route("/")
 def index():
+    _kawayi("xxx")
     return "kawayi"
+
+
+def img_path(p):
+    return os.path.join(image_folder, p)
+
+
+def cal(_uuid):
+    return anime(img_path(_uuid+".png"), img_path(_uuid+"_anime.png"))
+
+
+# 以下は、試しに動かしてみる
+# return static image path
+def _kawayi(fpath: str) -> str:
+    pass
